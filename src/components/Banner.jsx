64ee5e77e5e41c3e93e5d39c643dec435e49fffb
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-15 23:18:43 +0600

    Banner JSX Formatted and Updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -15,7 +15,6 @@ const Banner = () => {
           </p>
           <div>
             <button
-              // onClick={() => addPlayerToQueue(player)}
               type="button"
               className="group flex h-14 w-[200px] items-center justify-center rounded-lg bg-gradient-to-r from-purple-500 via-red-500 to-yellow-500 p-[1.5px] text-white duration-300 hover:bg-gradient-to-l hover:shadow-2xl hover:shadow-purple-600/30"
             >
